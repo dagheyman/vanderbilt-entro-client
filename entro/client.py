69f2c4df30e20a90b6afb97c158f1da05e527325
Dag Heyman Kajevic <d-h-k@hey.com>
2022-10-07 20:37:18 +0200

    wip: add a debug method and some timestamp logic

--- entro/client.py
+++ entro/client.py
@@ -1,6 +1,6 @@
 import requests
 import hashlib
-from datetime import datetime
+from datetime import datetime, timezone
 from dataclasses import dataclass
 
 
@@ -38,7 +38,7 @@ class EntroClient:
         resp_values = self.parse_response(r.text)
         self.session = resp_values[1]
 
-    def get_bookings(self):
+    def get_active_booking(self):
         r = self.s.get(
             self.url
             + "/showres.cgi?session="
@@ -47,15 +47,32 @@ class EntroClient:
             + self.OBJECT_ID
             + "&type=0"
         )
+
         resp_values = self.parse_response(r.text)
 
-        return [
-            Booking(
-                title=resp_values[1],
-                start=datetime.utcfromtimestamp(int(resp_values[2])),
-                end=datetime.utcfromtimestamp(int(resp_values[5])),
-            )
-        ]
+        return Booking(
+            title=resp_values[1],
+            start=datetime.utcfromtimestamp(int(resp_values[2])),
+            end=datetime.utcfromtimestamp(int(resp_values[5])),
+        )
+
+    def get_all_bookings():
+        # TODO: It's the same API call above, but with type=1
+        pass
+
+    # curl 'http://bokning.brfdammen.se:7999/makeres.cgi?session=0&object=1&start=1035028800&stop=1035037800'
+    # datetime to utc timestamp: int(x.replace(tzinfo=timezone.utc).timestamp())
+    # Response codes:
+    #
+    # RESERVATION_OK              0
+    # RESERVATION_ERROR           1
+    # RESERVATION_INTERVAL_FULL   2
+    # RESERVATION_FAMILY_FULL     3
+    # RESERVATION_NO_TIME         4
+    # RESERVATION_PERIOD_FULL     5
+    # RESERVATION_OBJECT_DISABLED 6
+    def make_booking():
+        pass
 
     @staticmethod
     def get_salted_pw(salt, password):
@@ -64,3 +81,56 @@ class EntroClient:
     @staticmethod
     def parse_response(data):
         return data.split()
+
+    @staticmethod
+    def date_to_timestamp(date):
+        return int(date.replace(year=2002, tzinfo=timezone.utc).timestamp())
+
+    def _debug(self):
+        print("showres.cgi (my bookings)")
+        r = self.s.get(
+            self.url
+            + "/showres.cgi?session="
+            + self.session
+            + "&object="
+            + self.OBJECT_ID
+            + "&type=0"
+        )
+        print(r.text)
+
+        print("showres.cgi (all bookings)")
+        r = self.s.get(
+            self.url
+            + "/showres.cgi?session="
+            + self.session
+            + "&object="
+            + self.OBJECT_ID
+            + "&type=1"
+        )
+        print(r.text)
+
+        print("time.cgi")
+        r = self.s.get(
+            self.url + "/time.cgi?session=" + self.session + "&object=" + self.OBJECT_ID
+        )
+        print(r.text)
+
+        print("combo.cgi")
+        r = self.s.get(
+            self.url
+            + "/combo.cgi?session="
+            + self.session
+            + "&object="
+            + self.OBJECT_ID
+        )  # need start and stop dates
+        print(r.text)
+
+        print("resdata.cgi")
+        r = self.s.get(
+            self.url
+            + "/resdata.cgi?session="
+            + self.session
+            + "&object="
+            + self.OBJECT_ID
+        )  # need start and stop dates and size
+        print(r.text)
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,6 +1,7 @@
 import pytest
 
 from entro.client import EntroClient
+from datetime import datetime
 
 
 def test_parse_response():
@@ -26,4 +27,16 @@ def test_get_salted_pw():
     assert (
         EntroClient.get_salted_pw("salt", "password")
         == "67a1e09bb1f83f5007dc119c14d663aa"
+    )
+
+
+def test_date_to_timestamp():
+
+    assert (
+        EntroClient.date_to_timestamp(datetime.fromisoformat("2022-10-11 17:00"))
+        == 1034355600
+    )
+    assert (
+        EntroClient.date_to_timestamp(datetime.fromisoformat("2022-10-11 19:30"))
+        == 1034364600
     )
